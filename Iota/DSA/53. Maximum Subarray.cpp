647add46639bdc710f5fd6fba355a324b103016e
being-iota <ratnapriya2954@gmail.com>
2024-10-07 19:00:57 +0530

    Updated 07 October 2024

---
+++ Iota/DSA/53. Maximum Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+     int max_current = nums[0];
+    int max_global = nums[0];
+
+    for (size_t i = 1; i < nums.size(); i++) {
+        max_current = max(nums[i], max_current + nums[i]);
+        if (max_current > max_global) {
+            max_global = max_current;
+        }
+    }
+
+    return max_global;
+    }
+};
