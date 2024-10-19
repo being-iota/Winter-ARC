716cd6dacabd683f14fae9381bc25d191c3c9901
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-10-19 15:22:57 +0530

    updated

--- Iota/DSA/153. Find Minimum in Rotated Sorted Array.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-      int n= nums.size();
-      sort(nums.begin(),nums.end());
-      int result =nums[0];
-      return result;
-    }
-};
